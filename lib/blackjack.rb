2ea0b60ec87149ed8050878a2e0537be9fe47839
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 12:31:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,8 @@ def hit?(card_total)
   user_input = get_user_input 
   if user_input == "h"
     card_total += deal_card
-    elsif user_input == "s"
+    elsif 
+    user_input == "s"
     return card_total
     
   end 
