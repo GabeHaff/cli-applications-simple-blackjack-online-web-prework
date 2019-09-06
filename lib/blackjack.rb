a464012650e3e1a0cae92f4db3265d428161cb1a
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 12:15:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,6 @@ def invalid_command
   puts "Sorry, not a valid command"
   get_user_input 
 end
-end 
 
 #####################################################
 # get every test to pass before coding runner below #
